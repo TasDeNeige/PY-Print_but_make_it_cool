9fd22ea0b144dfc193e72952066de2835de1efef
Ama ✨ <103329561+TasDeNeige@users.noreply.github.com>
2022-11-12 15:40:55 +0100

    Add files via upload

---
+++ Print but make it cool.py
@@ -0,0 +1,39 @@
+#Inspired by @achrafcodes on Instagram
+#Made by Amaryne B. (a.k.a. TasDeNeige), 12/11/2022
+
+from time import sleep
+
+def print_but_cool(sentence):
+    sentence = sentence.lower() #Prevent the program from not finding an uppercase character
+
+    #---------- DEFINE ALPHABET ----------#
+    alphabet = []
+    numASCIIlettre = 97
+    alphabet.append(" ") #Adds the 'space' character to the list
+    alphabet.append(".")
+
+    for i in range(26):
+        alphabet.append(chr(numASCIIlettre + i))
+
+    #---------- CREATE SENTENCE ----------#
+    letter = ""
+    newSentence = ""
+    numLetter = -1 #Used to go through the alphabet list, -1 to not get out of range
+
+    for i in range(len(sentence)):
+        while letter != sentence[i]:
+
+            if numLetter == 26:
+                letter = "*" #Replace an unknown character
+                break
+
+            else:
+                numLetter += 1
+                letter = alphabet[numLetter]
+                print(newSentence + letter)
+                sleep(0.05) #Gives this cool effect of printing
+
+        newSentence += letter
+        numLetter = -1 #Goes back to the beginning of the list
+
+print_but_cool(str(input("What do you want to print? -> ")))

a1ffc6595ea9e966069fab6aaede0fde1449d41d
Ama ✨ <103329561+TasDeNeige@users.noreply.github.com>
2022-12-03 11:40:35 +0100

    Restart update
    Adds a "Restart" function

--- Print but make it cool.py
+++ Print but make it cool.py
@@ -1,39 +1,50 @@
 #Inspired by @achrafcodes on Instagram
-#Made by Amaryne B. (a.k.a. TasDeNeige), 12/11/2022
+#Made by Amaryne B. (a.k.a. TasDeNeige), 12/11/2022, updated on 03/12/2022
 
 from time import sleep
+Restart = True
 
-def print_but_cool(sentence):
-    sentence = sentence.lower() #Prevent the program from not finding an uppercase character
+while Restart == True:
+    def print_but_cool(sentence):
+        sentence = sentence.lower() #Prevent the program from not finding an uppercase character
 
-    #---------- DEFINE ALPHABET ----------#
-    alphabet = []
-    numASCIIlettre = 97
-    alphabet.append(" ") #Adds the 'space' character to the list
-    alphabet.append(".")
+        #---------- DEFINE ALPHABET ----------#
+        alphabet = []
+        numASCIIlettre = 97
+        alphabet.append(" ") #Adds the 'space' character to the list
+        alphabet.append(".")
 
-    for i in range(26):
-        alphabet.append(chr(numASCIIlettre + i))
+        for i in range(26):
+            alphabet.append(chr(numASCIIlettre + i))
 
-    #---------- CREATE SENTENCE ----------#
-    letter = ""
-    newSentence = ""
-    numLetter = -1 #Used to go through the alphabet list, -1 to not get out of range
+        #---------- CREATE SENTENCE ----------#
+        letter = ""
+        newSentence = ""
+        numLetter = -1 #Used to go through the alphabet list, -1 to not get out of range
 
-    for i in range(len(sentence)):
-        while letter != sentence[i]:
+        for i in range(len(sentence)):
+            while letter != sentence[i]:
 
-            if numLetter == 26:
-                letter = "*" #Replace an unknown character
-                break
+                if numLetter == 26:
+                    letter = "*" #Replace an unknown character
+                    break
 
-            else:
-                numLetter += 1
-                letter = alphabet[numLetter]
-                print(newSentence + letter)
-                sleep(0.05) #Gives this cool effect of printing
+                else:
+                    numLetter += 1
+                    letter = alphabet[numLetter]
+                    print(newSentence + letter)
+                    sleep(0.05) #Gives this cool effect of printing
 
-        newSentence += letter
-        numLetter = -1 #Goes back to the beginning of the list
+            newSentence += letter
+            numLetter = -1 #Goes back to the beginning of the list
 
-print_but_cool(str(input("What do you want to print? -> ")))
+
+    print_but_cool(str(input("What do you want to print? -> ")))
+    print("\n")
+    Again = str(input("Print something else? (Y/N) -> ")).lower()
+
+    if Again == "n" or Again == "no":
+        Restart = False
+
+print("Goodbye! Hope you had fun!")
+sleep(2)
